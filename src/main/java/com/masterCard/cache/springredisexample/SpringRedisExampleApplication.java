ade352fdf6074ace8ea56a596ec057aa547f7af8
samarth.arun.narula <samarth.arun.narula@accenture.com>
2019-05-23 16:53:28 +0530

    second commit

---
+++ src/main/java/com/masterCard/cache/springredisexample/SpringRedisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.masterCard.cache.springredisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisExampleApplication.class, args);
+	}
+
+}
